37b7a8d1e0988e5fa2fa8e55968ed980b66033a7
Abraham411 <abrahamalausa04@gmail.com>
2025-04-04 09:09:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Adeniyi',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Adeniyi Bot",
+  botOwner: 'Adeniyi', // Your name
+  ownerNumber: '81423301162', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
